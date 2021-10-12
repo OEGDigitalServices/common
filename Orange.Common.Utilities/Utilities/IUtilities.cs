eb020538296327f72d51ac010dac9e4300c5ac09
Waleed Elwakeel <waleed_elwakeel13@hotmail.com>
2021-10-12 18:50:13 +0200

    Adding caching and perresolve registrations

--- Orange.Common.Utilities/Utilities/IUtilities.cs
+++ Orange.Common.Utilities/Utilities/IUtilities.cs
@@ -58,6 +58,7 @@ namespace Orange.Common.Utilities
         CultureInfo GetCultureInfo(string language);
         DateTime FormatDate(string date, string dateFormat);
         List<T> GetAllCachedRecordsFromDb<T>(string cacheKey, List<T> records);
+        List<T> GetAllCachedRecordsFromDb<T>(string cacheKey, Func<List<T>> fetchingMethod, double? daysToExpire = null);
         T Deserialize<T>(string json);
     }
 }
