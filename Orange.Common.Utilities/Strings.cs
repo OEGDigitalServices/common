3ddcf8f4475d7e714a0db15c2be0cebb3753b13a
Omar Hesham Abdellatif <omar.heabdellatif@orange.com>
2022-03-21 11:52:28 +0200

    Removing duplicate orderdescriptions

--- Orange.Common.Utilities/Strings.cs
+++ Orange.Common.Utilities/Strings.cs
@@ -538,12 +538,5 @@
         {
             public const string Authorization = "Authorization";
         }
-        public struct OrderDescriptions
-        {
-            public const string GSMRechargeOrderDescription = "MOGSMRecharge";
-            public const string GSMAddBillOrderDescription = "MOGSMAddBill";
-            public const string DSLRechargeOrderDescription = "MODSLRecharge";
-            public const string DSLAddBillOrderDescription = "MODSLAddBill";
-        }
     }
 }
