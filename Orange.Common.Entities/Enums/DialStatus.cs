75015e4474a03c4e5b861b939918ad4c52cdb53c
Waleed Elwakeel <waleed_elwakeel13@hotmail.com>
2022-03-23 20:49:43 +0200

    Adding dial utilities

---
+++ Orange.Common.Entities/Enums/DialStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orange.Common.Entities
+{
+    public enum DialStatus
+    {
+        Active,
+        SoftDisconnected,
+        VoluntarySuspended,
+        InvoluntarySuspended,
+        Inactive,
+        PermentlyDeactive,
+        Deactive,
+        Suspended,
+        Hold
+    }
+}
