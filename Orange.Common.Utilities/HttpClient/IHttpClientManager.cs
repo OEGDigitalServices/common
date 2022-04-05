bfa48307d116123e61a32a0efa407cf5c10b51f3
Peter Maher Ayoub <peter.ayoub@orange.com>
2022-04-05 11:39:58 +0200

    remove not needed Methods

--- Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
+++ Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
@@ -15,8 +15,5 @@ namespace Orange.Common.Utilities
 
         Task<object> PostAsJson<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null, int timeoutInSeconds = 100)
             where TBody : class;
-        //Task<T> PostXml<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null)
-        //    where TBody : class;
-        //Task<object> PostAsJson<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null) where TBody : class;
     }
 }
