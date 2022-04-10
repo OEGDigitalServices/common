5a6bd63751d9ab9713b51384845a282a1e71f74f
Waleed Elwakeel <waleed_elwakeel13@hotmail.com>
2022-04-10 16:50:01 +0200

    fix Merge issues

--- Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
+++ Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
@@ -16,9 +16,6 @@ namespace Orange.Common.Utilities
 
         Task<object> PostAsJson<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null, int timeoutInSeconds = 100)
             where TBody : class;    
-        Task<T> PostXml<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null)
-            where TBody : class;
-        Task<object> PostAsJson<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null) where TBody : class;
         Task<(HttpResponseMessage response, string stringContent)> GetWithoutSuccessEnsurance(string url, Dictionary<string, string> headers = null);
         Task<(HttpResponseMessage response, string stringContent)> PostWithoutSuccessEnsurance<TBody>(string url, TBody body, Dictionary<string, string> headers = null);
         Task<(HttpResponseMessage response, string stringContent)> PostWithoutSuccessEnsurance(string url, Dictionary<string, string> headers = null);
--- Orange.Common.Utilities/ServicesUtilities/IServicesUtilties.cs
+++ Orange.Common.Utilities/ServicesUtilities/IServicesUtilties.cs
@@ -27,6 +27,5 @@ namespace Orange.Common.Utilities
         ServiceCallOutput SendGatewayRequest(string url, string request, string requestVerb = Strings.Services.PostVerb, string headers = null);
         DialType GetDialType(string rpCode);
         bool IsStagingEnviroment();
-        DialType GetDialType(string rpCode);
     }
 }
