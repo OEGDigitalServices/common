0420eb235e1251fab00b14edca7225a600e06490
Waleed Elwakeel <waleed_elwakeel13@hotmail.com>
2022-06-21 20:42:18 +0200

    Adding NextTopUpViaWallet module name

--- Orange.Common.Entities/Enums/ModulesNames.cs
+++ Orange.Common.Entities/Enums/ModulesNames.cs
@@ -314,7 +314,7 @@
         HashOneHashOffer = 309,
         ActivateDial = 310,
         NextActivationViaWallet = 311,
-        RechargeViaWallet = 312
+        NextTopUpViaWallet = 312
     }
 }
 
