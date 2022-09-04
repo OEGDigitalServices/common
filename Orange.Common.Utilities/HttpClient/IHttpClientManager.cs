75a91df4177ac640d436c846457bdd341259ced0
Nour Almeslmy <nouralmeslmy@gmail.com>
2022-09-04 21:42:03 +0200

    Convert to captchaV2, fix captcha issues

--- Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
+++ Orange.Common.Utilities/HttpClient/IHttpClientManager.cs
@@ -6,7 +6,7 @@ namespace Orange.Common.Utilities
     public interface IHttpClientManager
     {
         Task<string> Get(string url, Dictionary<string, string> headers = null, int timeoutInSeconds = 100);
-        Task<T> Get<T>(string url, Dictionary<string, string> headers = null, int timeoutInSeconds = 100);
+        Task<T> Get<T>(string url, Dictionary<string, string> headers = null, int timeoutInSeconds = 100, bool disableSSL = false);
         Task<T> Post<T>(string url, Dictionary<string, string> headers = null, int timeoutInSeconds = 100);
         Task<T> Post<T, TBody>(string url, TBody body, Dictionary<string, string> headers = null, int timeoutInSeconds = 100)
             where TBody : class;
