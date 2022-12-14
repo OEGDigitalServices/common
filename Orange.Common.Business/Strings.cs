366a78678c553e750747096bf60333a9757d8ea1
gamal salem <gamal.sasayed@orange.com>
2022-12-14 14:19:31 +0200

    Adding TP - Identify User - Attribute

--- Orange.Common.Business/Strings.cs
+++ Orange.Common.Business/Strings.cs
@@ -63,5 +63,15 @@
         {
             public const string Corporate = "corporate";
         }
+        public struct APIs
+        {
+            public const string CommonSecureIntegrationBusURL = "CommonSecureIntegrationBusURL";
+            public const string OrangeTriplePlayIdentifyUser = "Account/IdentifyUser";
+        }
+        public struct Verbs
+        {
+            public const string Post = "POST";
+
+        }
     }
 }
